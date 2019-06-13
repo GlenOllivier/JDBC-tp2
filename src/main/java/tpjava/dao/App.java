280c8302c8b1c8c8142117b2f04110c8263a0239
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-06-13 12:09:41 +0200

    Ajout de PersistenceManager

--- src/main/java/tpjava/dao/App.java
+++ src/main/java/tpjava/dao/App.java
@@ -1,8 +1,36 @@
 package tpjava.dao;
 
+import tpjava.dao.dal.PersistenceManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
 
     public static void main(String[] args) {
+        try {
+            Connection connection = PersistenceManager.getConnection();
 
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT * FROM t_contact_con")) {
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("con_email"));
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            System.out.println("Attention :" + e.getMessage());
+        }
+
+        try {
+            PersistenceManager.closeConnection();
+        } catch (SQLException e) {
+            System.out.println("Attention :" + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/tpjava/dao/dal/PersistenceManager.java
@@ -0,0 +1,46 @@
+package tpjava.dao.dal;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static sun.plugin.javascript.navig.JSType.URL;
+
+public class PersistenceManager {
+
+    private final static int CONNECTION_TIMEOUT = 10;
+
+    private static Connection connection;
+
+    private PersistenceManager() {}
+
+    public static Connection getConnection() throws SQLException {
+
+        if (connection == null || connection.isClosed() || !connection.isValid(CONNECTION_TIMEOUT)) {
+
+            Properties properties = new Properties();
+            try (FileInputStream fis = new FileInputStream("resources/conf.properties")) {
+                properties.load(fis);
+            }
+            catch (IOException e) {
+                System.out.println(e.getCause());
+            }
+
+            String login = properties.getProperty("jdbc.login");
+            String password = properties.getProperty("jdbc.password");
+            String url = properties.getProperty("jdbc.url");
+
+            connection = DriverManager.getConnection(url, login, password);
+        }
+        return connection;
+    }
+
+    public static void closeConnection () throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
